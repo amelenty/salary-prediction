6205c1be80cd4f9cfa4e080d5f967b9dfb888e2f
amelenty <ada.melentyeva@gmail.com>
2018-10-14 13:18:00 +0300

    add CSV file creation to the loop

--- parse_data.sh
+++ parse_data.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+mkdir output
+CSV_HEADER="date,median_salary\n"
+
 for file in $( ls data ); do
 	echo Processing $file
+	echo -e $CSV_HEADER > output/"${file%.*}-J.csv"
+	echo -e  $CSV_HEADER > output/"${file%.*}-M.csv"
+	echo -e  $CSV_HEADER > output/"${file%.*}-S.csv"
+	gawk -f parse.awk $file
+	echo Done.
 done
