f33ca943dc9f625cc7de137b5ab79e1620921e80
amelenty <ada.melentyeva@gmail.com>
2018-10-15 14:06:54 +0300

    add ARIMA model computation

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import pyramid.arima as pm
 
 JOB_TITLES = ['Junior Software Engineer', 'Software Engineer', 'Senior Software Engineer']
 
@@ -25,4 +26,12 @@ for file in data_files:
 
 all_data = pd.DataFrame.from_dict(data_dict, orient='index')
 se_data = all_data.loc[:, (slice(None), JOB_TITLES)]
+se_data = se_data.dropna(axis=1)
 
+se_data.index = pd.to_datetime(se_data.index)
+
+train, test = se_data.loc[:'2017-05', :], se_data.loc['2017-05':, :]
+
+model_dict = {}
+for column in train.columns:
+    model_dict[column] = pm.auto_arima(train[column], start_p=1, start_q=1, max_p=3, max_q=3, m=2, start_P=0, seasonal=False, trace=True, error_action='ignore', suppress_warnings=True, stepwise=True)
